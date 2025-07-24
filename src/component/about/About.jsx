2993ddb3a3bc48239759837eaf0c432210a9b641
Nabil Islam Hamja <hamjanabil11@gmail.com>
2025-07-25 04:09:44 +0600

    about component complete

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
-import code from '../../assets/code.gif'
+import code from '../../assets/code.gif';
 
 const About = () => {
     return (
         <div className='py-[149px] '>
-            <div className="flex justify-between py-[91px] px-[77px] bg-white/10">
+            <div className=" relative flex justify-between py-[91px] px-[77px] bg-white/10">
                 <div className="w-[40%] ">
                     <p className='font-primary  font-medium   text-[17px] text-white'>About Me</p>
                     <h2 className='font-primary font-bold text-[32px] pt-[8px] pb-[16px] text-white   '>Professional <span className='text-[#98c45d]'> Problem Solutions </span>
@@ -44,10 +44,21 @@ const About = () => {
                         </div>
                 </div>
                 <div className="w-[10%] text-[#58c4dc] "><FaReact size={100} /></div>
-                <div className="w-[30%] ">
-                    <img src={code} alt="" />
+                <div className="w-[30%]  ">
+                    <img className='w-full h-full  ' src={code} alt="" />
+                     <div className="text-white ">
+                        <div className=" absolute top-[192px] right-[396px] bg-gray-400 rounded-full     py-4 px-3.5 ">
+                            <h3 className='font-semibold font-secondary text-[16px] text-black  ' >MERN Stack Developer</h3>
+                        </div>
+                        <div className=" absolute top-[292px] right-[396px] bg-gray-400 rounded-full     py-4 px-3.5 ">
+                            <h3 className='font-semibold font-secondary text-[16px] text-black  ' >Nabil Islam Hamza</h3>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
+                   
 
         </div>
     )
