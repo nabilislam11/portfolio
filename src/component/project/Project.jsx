43195a0307c9153f71f835c474b149900dc5da13
Nabil Islam Hamja <hamjanabil11@gmail.com>
2025-08-05 04:45:37 +0600

    github logo add

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -3,6 +3,7 @@ import { TiLocationArrowOutline } from "react-icons/ti";
 import project1 from '../../assets/project1.png'
 import project2 from '../../assets/project2.png'
 import project3 from '../../assets/project3.png'
+import { IoLogoGithub } from 'react-icons/io5';
 const Project = () => {
     return (
         <div id='Project' className=' py-[98px] ' >
@@ -24,8 +25,13 @@ const Project = () => {
                         <p className='font-secondary   font-medium   text-[18px] text-purple-500  '>Web Design</p>
                         <h2 className=' font-primary font-bold text-[42px]  text-white pt-[12px] pb-[17px] '>Petroil-- a landing page</h2>
                         <p className='font-primary  font-medium   text-[16px] text-white pb-[55px] ' > petroil is a simple landing page with React and tailwind.</p>
-                        <div className="w-[49px] h-[49px] rounded-full flex items-center justify-center  group-hover:bg-purple-800  group-hover:text-white   bg-white/10 border border-white/10 text-purple-500 ">
-                         <a target='blank' href="https://petrol-react-project.vercel.app/">   <TiLocationArrowOutline size={35} /></a>
+                          <div className="flex  items-center gap-x-[20px]">
+                            <div className="w-[52px] h-[52px] rounded-full flex items-center justify-center  group-hover:border-purple-600 bg-white/10 border border-white/10 ">
+                                   <a target='blank' href="https://github.com/nabilislam11"> <IoLogoGithub className='size-9  text-white' /></a>
+                            </div>
+                        <div className="w-[49px] h-[49px] rounded-full flex items-center justify-center group-hover:bg-purple-800  group-hover:text-white   bg-white/10 border border-white/10 text-purple-500 ">
+                            <TiLocationArrowOutline size={35} />
+                        </div>
                         </div>
                     </div>
                 </div>
@@ -37,8 +43,13 @@ const Project = () => {
                         <p className='font-secondary   font-medium   text-[18px] text-purple-500'>Web Design</p>
                         <h2 className=' font-primary font-bold text-[42px]  text-white pt-[12px] pb-[17px] '>ShopHub -- a landing page</h2>
                         <p className='font-primary  font-medium   text-[16px] text-white pb-[55px] ' >"ShopHub - Modern E-commerce Landing Page for Trending Products" </p>
+                        <div className="flex  items-center gap-x-[20px]">
+                            <div className="w-[52px] h-[52px] rounded-full flex items-center justify-center  group-hover:border-purple-600 bg-white/10 border border-white/10 ">
+                                   <a target='blank' href="https://github.com/nabilislam11"> <IoLogoGithub className='size-9  text-white' /></a>
+                            </div>
                         <div className="w-[49px] h-[49px] rounded-full flex items-center justify-center group-hover:bg-purple-800  group-hover:text-white   bg-white/10 border border-white/10 text-purple-500 ">
                             <TiLocationArrowOutline size={35} />
+                        </div>
                         </div>
                     </div>
                 </div>
@@ -63,8 +74,13 @@ const Project = () => {
                         <p className='font-primary  font-medium   text-[16px] text-white pb-[55px] ' >“TCR World” is a high-performance racing event landing page built with React and Tailwind CSS. It features a dynamic countdown, bold visual aesthetics, and a responsive UI designed to attract users to explore upcoming race events. Deployed seamlessly on Vercel for production-level performance.
 
                         </p>
-                        <div className="w-[49px] h-[49px] rounded-full  flex items-center justify-center group-hover:bg-purple-800  group-hover:text-white   bg-white/10 border border-white/10 text-purple-500 ">
+                          <div className="flex  items-center gap-x-[20px]">
+                            <div className="w-[52px] h-[52px] rounded-full flex items-center justify-center  group-hover:border-purple-600 bg-white/10 border border-white/10 ">
+                                   <a target='blank' href="https://github.com/nabilislam11"> <IoLogoGithub className='size-9  text-white' /></a>
+                            </div>
+                        <div className="w-[49px] h-[49px] rounded-full flex items-center justify-center group-hover:bg-purple-800  group-hover:text-white   bg-white/10 border border-white/10 text-purple-500 ">
                             <TiLocationArrowOutline size={35} />
+                        </div>
                         </div>
                     </div>
                 </div>
